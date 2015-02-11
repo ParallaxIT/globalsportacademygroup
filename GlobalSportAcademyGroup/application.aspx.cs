01d56782d356d6939e08f42cc458adc6d478eff9
parallaxit <rsutherland@thedrillbook.com>
2015-02-11 16:48:44 -0700

    feb 11

--- GlobalSportAcademyGroup/application.aspx.cs
+++ GlobalSportAcademyGroup/application.aspx.cs
@@ -49,6 +49,9 @@ namespace GlobalSportAcademyGroup
 
             if (intRes > 0)
             {
+
+                bool res = ClsUtilities.MailSendSharedArticleNotificationToParent(studentRecord);
+
                 Response.Redirect("success.aspx");
             }
 
---
+++ GlobalSportAcademyGroup/classes/ClsGlobalscs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace GlobalSportAcademyGroup.classes
+{
+    public class ClsGlobalscs
+    {
+
+        public static string sgpw = "Enigma!2517";
+        public static string sgun = "globalacademy";
+
+        //if LIVE
+        public static string WebsiteURL
+        {
+            get { return ConfigurationManager.AppSettings["WebsiteURL"]; }
+        }
+
+    }
+}
